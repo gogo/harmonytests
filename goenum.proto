687209eadbe13e594908deaaeb36290c5bc10513
Walter Schulze <awalterschulze@gmail.com>
2014-11-26 17:47:19 +0200

    updated license

--- goenum.proto
+++ goenum.proto
@@ -1,5 +1,4 @@
 // Copyright (c) 2013, Vastech SA (PTY) LTD. All rights reserved.
-// http://code.google.com/p/gogoprotobuf/gogoproto
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
